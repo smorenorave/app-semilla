1b54b818b293fefd7785dd637103495cdf95c73b
smorenorave <smorenor@eafit.edu.co>
2022-03-24 14:23:13 -0500

    inicial commit

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <img alt="Vue logo" src="./assets/logo.png">
+  <SeedPlot msg="HOLA"/>
+</template>
+
+<script>
+import SeedPlot from './components/SeedPlot.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SeedPlot
+  }
+}
+</script>
+
+<style>
+#app {
+  padding-right: 400px;
+  padding-left: 400px;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ src/components/SeedPlot.vue
@@ -0,0 +1,109 @@
+<template>
+  <div class="hello">
+    <h2>Bienvenido al simulador de siembra</h2>
+
+    <div>
+      <form @submit="sendData" method="post">
+        <h5>¿Qué materia prima desea cosechar?</h5>
+
+        <div class="fr-form" >
+          <b-form-select
+            id="formSeed"
+            v-model="selectedSeed"
+            :options="optionsSeed"
+          ></b-form-select>
+        </div>
+
+  
+
+        <h5>¿En cuál parcerla desea sembrar?</h5>
+
+        <div class="fr-form">
+          <b-form-select
+            id="formPlot"
+            v-model="selectedPlot"
+            :options="optionsPlot"
+          ></b-form-select>
+        </div>
+        
+        <p>
+          <b-card>
+            
+          <input  class="btn btn-outline-success fr-botton"  type="submit" value="Enviar" />
+          
+          <input  class="btn btn-outline-danger fr-botton"  type="reset" value="Borrar" />
+          </b-card>
+        </p>
+      </form>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+
+  data() {
+    return {
+      texto: "hola",
+      selectedSeed: null,
+      optionsSeed: [
+        { value: null, text: "Seleccione una semilla" },
+        { value: "CORN", text: "Café" },
+        { value: "SUGAR", text: "Caña de azúcar" },
+        { value: "SOYBEAN", text: "Soya" },
+      ],
+
+      selectedPlot: null,
+      optionsPlot: [
+        { value: null, text: "Seleccione una parcela" },
+        { value: "107019", text: "Frontino" },
+        { value: "106977", text: "Girardota" },
+        { value: "106956", text: "La ceja" },
+      ],
+
+    };
+  },
+
+  methods:{
+    sendData:function(){
+      alert(JSON.stringify(this.selectedSeed+this.selectedPlot))
+      
+    }
+
+  }
+
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+
+a {
+  color: #42b983;
+}
+
+.fr-botton {
+  margin-left: 10px;
+  margin-right: 10px;
+ 
+}
+
+.fr-form {
+  margin-left: 3px;
+  margin-right: 3px;
+}
+</style>
